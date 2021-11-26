59ececcefffbc9bface59aa253ec3707bb5b6795
Aditya1803 <adityatomar1803@gmail.com>
2021-11-26 22:44:03 +0530

    first commit in tictactoe game

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      {/* react expects a single element. This whole code is inside component knows as Fragments, thats why there 
+      is no error. */}
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </>
+  );
+};
+
+export default App;

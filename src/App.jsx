8bdbeb8bc611cc944ea4e70b6409e3b40bbd617e
Aditya1803 <adityatomar1803@gmail.com>
2021-11-26 23:46:19 +0530

    second commit, edit sass and get to know styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
+import Boards from './components/Boards';
 
 const App = () => {
   return (
-    <>
+    <div className="app">
       {/* react expects a single element. This whole code is inside component knows as Fragments, thats why there 
       is no error. */}
-      <h1>Welcome to React Vite Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-    </>
+      <h1>TIC TAC TOE</h1>
+      <Boards />
+    </div>
   );
 };
 

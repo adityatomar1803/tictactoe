314c4f65e743b24cba2257c99b37c2db5bb8e06a
Aditya1803 <adityatomar1803@gmail.com>
2021-12-06 14:15:48 +0530

    wrote the logic for selecting the winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Boards from './components/Boards';
+import { calculateWinner } from './helper';
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setX] = useState(false);
+
+  const squareClickHandler = pos => {
+    if (board[pos] || winner) return;
+    setBoard(prev => {
+      return prev.map((square, map_pos) => {
+        if (map_pos === pos) {
+          return isXNext ? 'X' : '0';
+        }
+        return square;
+      });
+    });
+    setX(!isXNext);
+  };
+
+  const winner = calculateWinner(board);
+  console.log(winner);
+
+  const message = winner
+    ? `The winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : '0'}`;
+
   return (
     <div className="app">
       {/* react expects a single element. This whole code is inside component knows as Fragments, thats why there 
       is no error. */}
       <h1>TIC TAC TOE</h1>
-      <Boards />
+      <h2>{message}</h2>
+      <Boards board={board} squareClickHandler={squareClickHandler} />
     </div>
   );
 };

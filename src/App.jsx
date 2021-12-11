b85236b201e3ca4fd606dcca654b97cac512540e
Aditya1803 <adityatomar1803@gmail.com>
2021-12-11 13:01:53 +0530

    new state about game history

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,45 @@ import Boards from './components/Boards';
 import { calculateWinner } from './helper';
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setX] = useState(false);
+  // const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([
+    {
+      board: Array(9).fill(null),
+      isXNext: true,
+    },
+  ]);
+
+  const [currentMove, setCurerntMove] = useState(0);
+
+  const current = history[currentMove];
+
+  // const [isXNext, setX] = useState(false);
+  console.log('history', history);
 
   const squareClickHandler = pos => {
-    if (board[pos] || winner) return;
-    setBoard(prev => {
-      return prev.map((square, map_pos) => {
+    if (current.board[pos] || winner) return;
+
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, map_pos) => {
         if (map_pos === pos) {
-          return isXNext ? 'X' : '0';
+          return last.isXNext ? 'X' : '0';
         }
         return square;
       });
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
-    setX(!isXNext);
+    // setX(!isXNext);
+    setCurerntMove(prev => prev + 1);
   };
 
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(current.board);
   console.log(winner);
 
   const message = winner
     ? `The winner is ${winner}`
-    : `Next player is ${isXNext ? 'X' : '0'}`;
+    : `Next player is ${current.isXNext ? 'X' : '0'}`;
 
   return (
     <div className="app">
@@ -32,7 +49,7 @@ const App = () => {
       is no error. */}
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
-      <Boards board={board} squareClickHandler={squareClickHandler} />
+      <Boards board={current.board} squareClickHandler={squareClickHandler} />
     </div>
   );
 };
